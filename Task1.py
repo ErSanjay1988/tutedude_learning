c1e12d5e2689955e5f1cbc6c8db3a690515a2e5c
ErSanjay1988 <er.sanjaypandey@yahoo.co.in>
2025-07-29 18:26:37 +0530

    Delete Task1.py

--- Task1.py
+++
@@ -1,8 +0,0 @@
-# getting input from user
-a=int(input("Enter the first number: "))
-b=int(input("Enter the second number: "))5
-
-print("Addition :" ,a+b)
-print("Subtraction :", a-b)
-print("Multiplication :",a*b)
-print("Division :",a/b)
